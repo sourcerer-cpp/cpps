df7ddbfd0235b890631eced9e2c9ffb86047fc3a
Kishorirani <kishorirani108@gmail.com>
2022-06-25 20:01:01 +0530

    Learn arithmetic operations

--- main.cpp
+++ main.cpp
@@ -4,23 +4,15 @@
 
 int main()
 {
-    std::string greeting = "hello Kishori!\n\n";
-    std::cout << greeting;
-
-    int x = 42;
-
-    std::cout << x << "\n\n";
-
-    [[maybe_unused]] float pi = 3.14;
-    std::cout << pi  << "\n\n";
-
-    double my_very_awesome_variable = 2.5;
-    std::cout << my_very_awesome_variable << "\n\n";
-
-    char symbol = '@';
-    std::cout << symbol << "\n\n";
-
-    bool flag = false;
-    std::cout << flag << "\n\n";
+    int a = 5;
+    int b = 2;
+    int result = a + b;
+    std::cout << "a = " << a;
+    std::cout << "\nb = " << b;
+    std::cout << "\na + b = " << result << "\n";
+    int diff = a - b;
+    std::cout << "a - b = " << diff << '\n';
+    int product = a * b;
+    int quotient = a / b;
 }
 
