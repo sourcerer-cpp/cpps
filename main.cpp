b7d1f6989d6919b506d57cf41ac39f2cd5334aa0
Dino Source <sourcerer.cpp@gmail.com>
2022-07-04 18:49:20 +0400

    Add new topic: pointers, variable address

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 
 
-int main() {
-    std::cout << "Please enter an integer: "; // output message to console
-    int number = 0; // declare integer and initialize it to 0
-    std::cin >> number; // get integer from user
-    std::cout << "Current value of your number: " << number << '\n';
+int main()
+{
+    int kids = 4;
+    std::cout << "Value of variable kids: " << kids << '\n';
+    std::cout << "Address of variable kids: " << &kids << "\n\n";
 
-    char user_choice = 'y'; // declare variable of type char
-    while (user_choice == 'y') { // compare user_choice with character y
-        std::cout << "Would you like to increase your number? (y/n) ";
-        std::cin >> user_choice; // get character from user
+    int * pkids // pointer to kids (declaration of variable "pointer to int")
+        = &kids;// address of variable kids (we use & symbol to represent address)
 
-        if (user_choice == 'y') { // compare user_choice with character y
-            ++number; // increase (increment) number by 1
-        }
-        std::cout << "Current value of your number: " << number << '\n';
-        std::cout << "Would you like to proceed? (y/n) ";
-        std::cin >> user_choice; // get character from user
-    }
+    std::cout << "Value of variable pkids: " << pkids << '\n'; // pointer name is always an address of something else
+    std::cout << "Address of variable pkids: " << &pkids << "\n\n";
+
+    std::cout << "Value of variable pkids points to (i.e. value of kids): " << *pkids << '\n'; // dereferencing a pointer
+    std::cout << "Address of variable pkids points to (i.e. address of kids): " << &(*pkids) << '\n';
 }
+
