28863899dbfc10ab15a0f87bf1238d5e83726dfb
Dino Source <sourcerer.cpp@gmail.com>
2022-07-04 18:23:51 +0400

    Add new topic: function overloading, int type limitations

--- main.cpp
+++ main.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
+#include <string>
 
+void print(const std::string & message) {
+    std::cout << message << '\n';
+}
 
-int main()
-{
+void print(int number, const std::string & message) { // overloaded function
+    std::cout << number << message << '\n';
+}
+
+void get_user_input(int & input) {
+    std::cin >> input;
+}
+
+void get_user_input(char & input) { // overloaded function
+    std::cin >> input;
+}
+
+int main() {
     int number = 0;
     char user_choice = 'y';
 
     while (user_choice == 'y') {
-        std::cout << "Please enter an integer: ";
-        std::cin >> number;
+        print("Please enter an integer: ");
+        get_user_input(number);
 
         if (number > 0) {
-            std::cout << number << " is a positive number.\n";
+            print(number, " is a positive number.");
         }
         else if (number < 0) {
-            std::cout << number << " is a negative number.\n";
+            print(number, " is a negative number.");
         }
         else {
-            std::cout << "The number zero is neither positive nor negative.\n";
+            print(number, " is neither positive nor negative.");
         }
 
-        std::cout << "Would you like to proceed? (y/n) ";
-        std::cin >> user_choice;
+        print("Would you like to proceed? (y/n) ");
+        get_user_input(user_choice);
     }
 }
+
+// Pay attention that you cannot use very big integers while
+// you are using this code example, because of limitations of
+// type int which we use here.
+// You can learn more about integer types in Stephen Prata's
+// book in Chapter 3 "Dealing with Data", page 68
