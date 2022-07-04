cdff469dfdf078bbeb60f2bb902ace0c31b42b0e
Dino Source <sourcerer.cpp@gmail.com>
2022-07-04 18:50:57 +0400

    Add new topic: references

--- main.cpp
+++ main.cpp
@@ -7,13 +7,10 @@ int main()
     std::cout << "Value of variable kids: " << kids << '\n';
     std::cout << "Address of variable kids: " << &kids << "\n\n";
 
-    int * pkids // pointer to kids (declaration of variable "pointer to int")
-        = &kids;// address of variable kids (we use & symbol to represent address)
+    // declaration of variable ref_kids of type "reference to int":
+    int & ref_kids // ref_kids is just another name for kids (i.e. alias of kids)
+        = kids;
 
-    std::cout << "Value of variable pkids: " << pkids << '\n'; // pointer name is always an address of something else
-    std::cout << "Address of variable pkids: " << &pkids << "\n\n";
-
-    std::cout << "Value of variable pkids points to (i.e. value of kids): " << *pkids << '\n'; // dereferencing a pointer
-    std::cout << "Address of variable pkids points to (i.e. address of kids): " << &(*pkids) << '\n';
+    std::cout << "Value of variable ref_kids: " << ref_kids << '\n';
+    std::cout << "Address of variable ref_kids: " << &ref_kids << "\n\n";
 }
-
