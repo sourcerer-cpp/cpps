ba9adc23170012df56bb5115e68417314fb06b94
Dino Source <sourcerer.cpp@gmail.com>
2022-07-04 18:45:21 +0400

    Add new topic: operator++, operator==

--- main.cpp
+++ main.cpp
@@ -1,47 +1,22 @@
 #include <iostream>
-#include <string>
 
-void print(const std::string & message) {
-    std::cout << message << '\n';
-}
-
-void print(int number, const std::string & message) { // overloaded function
-    std::cout << number << message << '\n';
-}
-
-void get_user_input(int & input) {
-    std::cin >> input;
-}
-
-void get_user_input(char & input) { // overloaded function
-    std::cin >> input;
-}
 
 int main() {
-    int number = 0;
-    char user_choice = 'y';
+    std::cout << "Please enter an integer: "; // output message to console
+    int number = 0; // declare integer and initialize it to 0
+    std::cin >> number; // get integer from user
+    std::cout << "Current value of your number: " << number << '\n';
 
-    while (user_choice == 'y') {
-        print("Please enter an integer: ");
-        get_user_input(number);
+    char user_choice = 'y'; // declare variable of type char
+    while (user_choice == 'y') { // compare user_choice with character y
+        std::cout << "Would you like to increase your number? (y/n) ";
+        std::cin >> user_choice; // get character from user
 
-        if (number > 0) {
-            print(number, " is a positive number.");
+        if (user_choice == 'y') { // compare user_choice with character y
+            ++number; // increase (increment) number by 1
         }
-        else if (number < 0) {
-            print(number, " is a negative number.");
-        }
-        else {
-            print(number, " is neither positive nor negative.");
-        }
-
-        print("Would you like to proceed? (y/n) ");
-        get_user_input(user_choice);
+        std::cout << "Current value of your number: " << number << '\n';
+        std::cout << "Would you like to proceed? (y/n) ";
+        std::cin >> user_choice; // get character from user
     }
 }
-
-// Pay attention that you cannot use very big integers while
-// you are using this code example, because of limitations of
-// type int which we use here.
-// You can learn more about integer types in Stephen Prata's
-// book in Chapter 3 "Dealing with Data", page 68
